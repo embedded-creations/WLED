2bfbea876d3e4e3fd3fcc75f652dbead4e4b658b
Louis Beaudoin <louis@embedded-creations.com>
2021-01-26 17:07:42 +0000

    Add optional NpbWrapperFastLED.h that replaces NeoPixelBus with FastLED - wrapper can be included by setting path with CUSTOM_NPBWRAPPER_H - not all functionality of the wrapper has been tested

--- wled00/my_config_sample.h
+++ wled00/my_config_sample.h
@@ -9,4 +9,12 @@
  *   edit "my_config.h" directly, or copy the contents of one of the "my_config_sample*.h" files into "my_config.h" and edit as neeeded
  */
 
+// force the compiler to show a warning to confirm that this file is included (comment this out to remove warning)
+#warning **** my_config.h: Settings from this file are honored ****
+
 //#define MAX_LEDS 1500       //Maximum total LEDs. More than 1500 might create a low memory situation on ESP8266.
+
+//#define LEDPIN 27           // customize which pin to use to drive Neopixels
+
+// Optionally use custom NpbWrapper with FastLED backend instead of NeoPixelBus
+//#define CUSTOM_NPBWRAPPER_H "NpbWrapperFastLED.h"
